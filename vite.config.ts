11f2480fcd2dc48364ad3356db7209e82862a61f
Alexey Korepanov <alexey.korepanov@clickhouse.com>
2025-12-09 21:54:56 +0100

    Add GitHub Actions workflow for deploying to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/no-hidden-files/',
   test: {
     globals: true,
     environment: 'jsdom',
